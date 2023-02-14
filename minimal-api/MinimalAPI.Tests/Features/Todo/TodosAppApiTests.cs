29fbb5c2105629fdebbf1887a082e6b4537551b4
Kaveh Hadjari <kaveh.hadjari@gmail.com>
2023-02-15 00:40:17 +0100

    isolate in-memory database in unit tests

--- minimal-api/MinimalAPI.Tests/Features/Todo/TodosAppApiTests.cs
+++ minimal-api/MinimalAPI.Tests/Features/Todo/TodosAppApiTests.cs
@@ -8,21 +8,34 @@ namespace MinimalAPI.Tests.Features.Todo;
 public class TodosAppApiTests
 {
 
-    private readonly AppDbContext _dbContext =
-        new(
-            new []{ new TodoModelBuilder() },
-            new DbContextOptionsBuilder<AppDbContext>()
-                .UseInMemoryDatabase(databaseName: "MinimalAPI")
-                .Options
-        );
+    private class TestDbContextHolder : IDisposable
+    {
+        public AppDbContext DbContext { get; }
+        public TestDbContextHolder()
+        {
+            DbContext =  new AppDbContext(
+                new []{ new TodoModelBuilder() },
+                new DbContextOptionsBuilder<AppDbContext>()
+                    .UseInMemoryDatabase(databaseName: "MinimalAPI")
+                    .Options
+            );
+        }
+        public void Dispose()
+        {
+            DbContext.Database.EnsureDeleted();
+            DbContext.Dispose();
+        }
+    }
 
     [Fact]
     public async Task GetTodosTest()
     {
         //Arrange setup dbContext
+        using var dbContextHolder = new TestDbContextHolder();
+        var dbContext = dbContextHolder.DbContext;
         
         //Act
-        var results = (Ok<List<TodoListDto>>) await TodosAppApi.GetTodos(_dbContext);
+        var results = (Ok<List<TodoListDto>>) await TodosAppApi.GetTodos(dbContext);
         
         //Assert
         Assert.Equal(200, results.StatusCode);
@@ -32,8 +45,12 @@ public class TodosAppApiTests
     [Fact]
     public async Task CreateNewTodoTests()
     {
+        //Arrange
+        using var dbContextHolder = new TestDbContextHolder();
+        var dbContext = dbContextHolder.DbContext;
+        
         //Act
-        var results = (Ok<TodoList>) await TodosAppApi.CreateTodoList(_dbContext,
+        var results = (Ok<TodoList>) await TodosAppApi.CreateTodoList(dbContext,
             new NewTodoListDto("test-list", 
                 new List<NewTodoItem>(new[] { new NewTodoItem("test-item", false) })));
         
@@ -49,14 +66,16 @@ public class TodosAppApiTests
     public async Task GetTodo()
     {
         //Arrange
+        using var dbContextHolder = new TestDbContextHolder();
+        var dbContext = dbContextHolder.DbContext;
 
         var todoList = new TodoList("test-list");
         todoList.Todos.Add(new TodoList.Todo("item1", true));
-        _dbContext.Set<TodoList>().Add(todoList);
-        await _dbContext.SaveChangesAsync();
+        dbContext.Set<TodoList>().Add(todoList);
+        await dbContext.SaveChangesAsync();
         
         //Act
-        var results = (Ok<TodoListDto>) await TodosAppApi.GetTodo(_dbContext, 1); //assuming initial id is 1
+        var results = (Ok<TodoListDto>) await TodosAppApi.GetTodo(dbContext, 1); //assuming initial id is 1
         
         //Assert
         Assert.Equal(200, results.StatusCode);
